ca464fdd4b24c2f790a9201c72f9d3aa7d14ab51
Pau Fernández <pau.fernandez@upc.edu>
2012-02-26 15:54:38 +0100

    Convierto en package + version usable

--- fragments.go
+++ fragments.go
@@ -1,13 +1,14 @@
 
-package main
+package fragments
 
 import (
-	"os"
 	"io"
-	"bytes"
 	"fmt"
+	"time"
+	"bytes"
 	"strings"
 	"html/template"
+	"encoding/json"
 )
 
 type UpdateFunc func(id string) string
@@ -24,13 +25,20 @@ func init() {
 type Fragment struct {
 	kind     string
 	id       string
+	stamp    time.Time
+	valid    bool
 	text     *template.Template
 	fn       UpdateFunc
 	children []*Fragment
 }
 
 func (frag *Fragment) update(fn UpdateFunc) {
-	children := make([]string, 0)
+	// call update function
+	res := fn(frag.id) 
+	frag.stamp = time.Now()
+
+	// collect children + insert placeholders
+	children := []string{}
 	fmap := map[string]interface{} {
 		"fragment": func(id string) string {
 			i := len(children)
@@ -38,25 +46,30 @@ func (frag *Fragment) update(fn UpdateFunc) {
 			return fmt.Sprintf("{{ index .children %d }}", i)
 		},
 	}
-	tmpl, err := template.New("frag").Funcs(fmap).Parse(fn(frag.id))
+	tmpl, err := template.New("frag").Funcs(fmap).Parse(res)
 	if err != nil {
 		fmt.Println(err)
 		panic(fmt.Sprintf("Template for '%s' has errors", frag.kind))
 	}
 	var text bytes.Buffer
 	tmpl.Execute(&text, nil)
+	
+	// compile final template
 	frag.text, err = template.New("frag").Parse(text.String())
 	if err != nil {
 		panic("Internal template error")
 	}
-	frag.children = make([]*Fragment, 0)
+	frag.valid = true
+
+	// Get fragments recursively
+	frag.children = make([]*Fragment, len(children))
 	for i := range children {
-		frag.children = append(frag.children, Get(children[i]))
+		frag.children[i] = Get(children[i])
 	}
 }
 
 func (frag *Fragment) Render(w io.Writer) {
-	children := make([]template.HTML, 0)
+	children := []template.HTML{}
 	for i := range frag.children {
 		child := frag.children[i]
 		var result bytes.Buffer
@@ -66,10 +79,50 @@ func (frag *Fragment) Render(w io.Writer) {
 	frag.text.Execute(w, Values { "children": children })
 }
 
+func (frag *Fragment) ID() string {
+	return fmt.Sprintf("%s:%s", frag.kind, frag.id)
+}
+
+type DiffItem struct { id, html string }
+
+func (d DiffItem) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]string { "id": d.id, "html": d.html })
+}
+
+func (frag *Fragment) Diff(since time.Time) []DiffItem {
+	fmt.Println("Diff:", since)
+	D := []DiffItem{}
+	if (frag.stamp.After(since)) {
+		stubs := make([]template.HTML, len(frag.children))
+		for i := range frag.children {
+			stubs[i] = frag.children[i].Stub()
+		}
+		var b bytes.Buffer
+		frag.text.Execute(&b, Values { "children": stubs })
+		D = append(D, DiffItem{ id: frag.ID(), html: b.String() })
+	}
+	// call recursively
+	for i := range frag.children {
+		d := frag.children[i].Diff(since)
+		for j := range d {
+			D = append(D, d[j])
+		}
+	}
+	return D
+}
+
+func (frag *Fragment) Stub() template.HTML {
+	div := fmt.Sprintf("<div fragment=\"%s:%s\"></div>", frag.kind, frag.id)
+   return template.HTML(div)
+}
+
+//////////////////////////////////////////////////////////////////////
+
 func Get(id string) *Fragment {
 	if frag, ok := cache[id]; ok {
-		fmt.Println("FROM CACHE!")
-		return frag
+		if frag.valid {
+			return frag
+		}
 	}
 	parts := strings.Split(id, ":")
 	if len(parts) != 2 {
@@ -79,48 +132,24 @@ func Get(id string) *Fragment {
 	if ! ok {
 		panic(fmt.Sprintf("Fragment type '%s' not found", parts[0]))
 	}
-	frag := &Fragment{ kind: parts[0], id: parts[1], fn: fn }
+	frag := &Fragment{ kind: parts[0], id: parts[1], fn: fn, valid: false }
 	frag.update(fn)
 	cache[id] = frag
 	return frag
 }
 
 func Ref(id string) template.HTML {
-	return template.HTML(fmt.Sprintf("{{ \"%s\" | fragment }}", id))
+	return template.HTML(fmt.Sprintf(`{{ "%s" | fragment }}`, id))
 }
 
-const T = `<div id="{{ .id }}" class="comments">
-<p>{{ .comment }}</p>
-by {{ .user }}
-</div>
-`
-
-func comments(id string) string {
-	cT, _ := template.New("comment").Parse(T)
-	var b bytes.Buffer
-	cT.Execute(&b, Values {
-		"id": id,
-		"comment": "Blah blah",
-		"user": Ref("user:pauek"),
-	})
-	return b.String()
-}
-
-func user(id string) string {
-	return fmt.Sprintf("<div class=\"user\">User: %s</div>", id)
-}
-
-func main() {
-	routes["comments"] = comments
-	routes["user"] = user
-
-	var frag *Fragment
-	frag = Get("comments:jarl")
-	frag.Render(os.Stdout)
-	frag = Get("comments:xxx")
-	frag.Render(os.Stdout)
-
-	for k, v := range cache {
-		fmt.Println(k, v)
+func Invalidate(id string) bool {
+	if frag, ok := cache[id]; ok {
+		frag.valid = false
+		return true
 	}
+	return false
+}
+
+func Add(id string, fn UpdateFunc) {
+	routes[id] = fn
 }
