461352a44cb473ab4f46dc0795ea16ac3b37b8a4
Pau Fernández <pau.fernandez@upc.edu>
2012-04-24 13:49:54 +0200

    Tipos + bug fix + invalidación
    - Introduzco el Type, para aclarar (espero).
    - Arreglo el Get de fragmentos inválidos dentro de otros válidos.
    - Tabla de invalidación.

--- fragments.go
+++ fragments.go
@@ -6,15 +6,14 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"os"
 	"strings"
 	"time"
 )
 
-type UpdateFunc func(id string) (string, error)
 type Values map[string]interface{}
 
-var funcs = make(map[string]UpdateFunc)
-var cache = make(map[string]*Fragment)
+type UpdateFunc func(id string) (string, error)
 
 type Fragment struct {
 	kind     string
@@ -22,10 +21,19 @@ type Fragment struct {
 	stamp    time.Time
 	valid    bool
 	text     *template.Template
-	fn       UpdateFunc
-	children []*Fragment
+	children []string
 	err      error
 }
+
+type Type struct {
+	updatefn UpdateFunc
+}
+
+var (
+	types  = make(map[string]Type)
+	cache  = make(map[string]*Fragment)
+	depend = make(map[string]map[string]bool)
+)
 
 func (frag *Fragment) update(fn UpdateFunc) {
 	// call update function
@@ -50,32 +58,27 @@ func (frag *Fragment) update(fn UpdateFunc) {
 		fmt.Println(err)
 		panic(fmt.Sprintf("Template for '%s' has errors", frag.kind))
 	}
-	var text bytes.Buffer
-	tmpl.Execute(&text, nil)
-
+	var b bytes.Buffer
+	tmpl.Execute(&b, nil)
+	text := fmt.Sprintf(`<div fragment="%s">%s</div>`, frag.ID(), b.String())
+	
 	// compile final template
-	frag.text, err = template.New("frag").Parse(text.String())
+	frag.text, err = template.New("frag").Parse(text)
 	if err != nil {
 		frag.err = err
 		return
 	}
 	frag.valid = true
-
-	// Get fragments recursively
-	frag.children = make([]*Fragment, len(children))
-	for i := range children {
-		frag.children[i], err = Get(children[i])
-		if err != nil {
-			frag.err = fmt.Errorf("Children %d fragment error: %s\n", i, err)
-			return
-		}
-	}
+	frag.children = children
 }
 
 func (frag *Fragment) Render(w io.Writer) {
 	children := []template.HTML{}
 	for i := range frag.children {
-		child := frag.children[i]
+		child, err := Get(frag.children[i])
+		if err != nil {
+			frag.err = fmt.Errorf("Children '%s' fragment error: %s\n", frag.children[i], err)
+		}
 		var result bytes.Buffer
 		child.Render(&result)
 		children = append(children, template.HTML(result.String()))
@@ -99,7 +102,11 @@ func (frag *Fragment) Diff(since time.Time) []DiffItem {
 	if frag.stamp.After(since) {
 		stubs := make([]template.HTML, len(frag.children))
 		for i := range frag.children {
-			stubs[i] = frag.children[i].Stub()
+			child, err := Get(frag.children[i])
+			if err != nil {
+				panic("Cannot get child!")
+			}
+			stubs[i] = child.Stub()
 		}
 		var b bytes.Buffer
 		frag.text.Execute(&b, Values{"children": stubs})
@@ -107,7 +114,11 @@ func (frag *Fragment) Diff(since time.Time) []DiffItem {
 	}
 	// call recursively
 	for i := range frag.children {
-		d := frag.children[i].Diff(since)
+		child, err := Get(frag.children[i])
+		if err != nil {
+			panic("Cannot get child!")
+		}
+		d := child.Diff(since)
 		for j := range d {
 			D = append(D, d[j])
 		}
@@ -122,6 +133,16 @@ func (frag *Fragment) Stub() template.HTML {
 
 //////////////////////////////////////////////////////////////////////
 
+// Register a type of fragment. The ID of the type is a prefix of the
+// fragment ID. For instance, a fragment 'user:pauek' has type 'user'.
+//
+func Add(typId string, fn UpdateFunc) {
+	types[typId] = Type{updatefn: fn}
+}
+
+// Get a fragment from cache. If it exists and is valid, it is
+// returned immediately.  Otherwise it is first updated.
+//
 func Get(id string) (F *Fragment, err error) {
 	if frag, ok := cache[id]; ok {
 		if frag.valid {
@@ -135,11 +156,12 @@ func Get(id string) (F *Fragment, err error) {
 	if len(parts) != 2 {
 		panic(fmt.Sprintf("Malformed fragment ID: '%s'", id))
 	}
-	fn, ok := funcs[parts[0]]
+	typ, ok := types[parts[0]]
 	if !ok {
 		panic(fmt.Sprintf("Fragment type '%s' not found", parts[0]))
 	}
-	frag := &Fragment{kind: parts[0], id: parts[1], fn: fn, valid: false}
+	fn := typ.updatefn
+	frag := &Fragment{kind: parts[0], id: parts[1], valid: false}
 	frag.update(fn)
 	cache[id] = frag
 	if frag.err != nil {
@@ -148,20 +170,37 @@ func Get(id string) (F *Fragment, err error) {
 	return frag, nil
 }
 
+// Create a reference to another fragment to put in a template.
+//
 func Ref(id string) template.HTML {
 	return template.HTML(fmt.Sprintf(`{{ "%s" | fragment }}`, id))
 }
 
-func Invalidate(id string) bool {
-	if frag, ok := cache[id]; ok {
-		frag.valid = false
-		return true
+// invalidate all fragments depending on the object with 'id'
+//
+func Invalidate(objId string) {
+	fmt.Fprintf(os.Stderr, "Invalidate(%s)\n", objId)
+	if fragIds, ok := depend[objId]; ok {
+		for fragId := range fragIds {
+			fmt.Fprintf(os.Stderr, "checking '%s'\n", fragId)
+			if frag, found := cache[fragId]; found {
+				fmt.Fprintf(os.Stderr, "Invalidate '%s' -> '%s'\n", objId, fragId)
+				frag.valid = false
+			}
+		}
 	}
-	return false
 }
 
-func Add(id string, fn UpdateFunc) {
-	funcs[id] = fn
+// Declare a dependency between an object with ID 'objId' and a
+// fragment with id 'fragId'. When Invalidate is called with 'objId',
+// all fragments with a Declared dependency will be updated.
+//
+func Depends(fragId, objId string) {
+	if _, ok := depend[objId]; !ok {
+		depend[objId] = make(map[string]bool)
+	}
+	depend[objId][fragId] = true
+	fmt.Fprintf(os.Stderr, "Depends[%s] = %v\n", objId, depend[objId])
 }
 
 /*
